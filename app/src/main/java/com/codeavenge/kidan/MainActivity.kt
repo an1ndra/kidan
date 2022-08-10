4134f7dfd9f581f690ecc91f174dfb1b5f36eb34
Anindra <anindrakarmakar@proton.me>
2022-08-10 14:16:42 +0530

    Testing purpose

--- app/src/main/java/com/codeavenge/kidan/MainActivity.kt
+++ app/src/main/java/com/codeavenge/kidan/MainActivity.kt
@@ -8,7 +8,6 @@ import com.codeavenge.kidan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
