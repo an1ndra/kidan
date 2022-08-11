74da1dc85aa549f57492437430637933948dfaf3
Anindra <anindrakarmakar@proton.me>
2022-08-11 22:43:44 +0530

    Add QuizFragment.kt

--- app/src/main/java/com/codeavenge/kidan/QuizFragment.kt
+++ app/src/main/java/com/codeavenge/kidan/QuizFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TempFragment.newInstance] factory method to
+ * Use the [QuizFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TempFragment : Fragment() {
+class QuizFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class TempFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_temp, container, false)
+        return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class TempFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TempFragment.
+         * @return A new instance of fragment QuizFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TempFragment().apply {
+            QuizFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
